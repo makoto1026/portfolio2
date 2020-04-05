9419314c3b79b5f0e25a58c8cbb5b549fb4e12c6
makoto1026 <trsmmkt@gmail.com>
2020-04-05 20:42:14 +0900

    deploy_commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  server '3.21.182.114', user: 'ec2-user', roles: %w{app db web}
+
 end
